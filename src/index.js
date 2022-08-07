e51cdfac406b9f092f56b73f6a7f838bc6b1097d
Mészáros Patrik <hulye31@gmail.com>
2022-08-07 09:35:05 +0200

    Test commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const { default: mongoose } = require('mongoose');
-const nunjucks = require('nunjucks');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const expressSession = require('express-session');
-const path = require('path');
-const MongoStore = require('connect-mongo');
-const { ensureAuthenticated } = require('./utils/ensureAuthenticated');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const { default: mongoose } = require("mongoose");
+const nunjucks = require("nunjucks");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const expressSession = require("express-session");
+const path = require("path");
+const MongoStore = require("connect-mongo");
+const { ensureAuthenticated } = require("./utils/ensureAuthenticated");
 
 const app = express();
 
@@ -16,51 +16,51 @@ const port = process.env.PORT || 3000;
 const connectionUri = process.env.MONGODB_CONNECTION_URI;
 const secret = process.env.SESSION_SECRET;
 
-const nunjuckEnv = nunjucks.configure('src/views', {
+const nunjuckEnv = nunjucks.configure("src/views", {
   autoescape: true,
   express: app,
-  watch: process.env.NODE_ENV === 'production' ? false : true,
+  watch: process.env.NODE_ENV === "production" ? false : true,
 });
 
-nunjuckEnv.addFilter('date', (dateInput) => {
+nunjuckEnv.addFilter("date", (dateInput) => {
   const date = new Date(dateInput);
-  return date.toISOString().split('T')[0].replaceAll('-', '. ') + '.';
+  return date.toISOString().split("T")[0].replaceAll("-", ". ") + ".";
 });
 
-app.set('engine', nunjuckEnv);
+app.set("engine", nunjuckEnv);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(connectionUri);
 
-mongoose.connection.on('connected', () => {
-  console.log('succesfull db connect');
+mongoose.connection.on("connected", () => {
+  console.log("succesfull db connect");
 });
 
-mongoose.connection.on('error', (error) => {
-  console.log('error', error);
+mongoose.connection.on("error", (error) => {
+  console.log("error", error);
 });
 
-require('./models/user.model');
-require('./models/country.model');
-require('./models/subcountry.model');
-require('./models/city.model');
-require('./models/image.model');
-require('./models/category.model');
-require('./models/rating.model');
+require("./models/user.model");
+require("./models/country.model");
+require("./models/subcountry.model");
+require("./models/city.model");
+require("./models/image.model");
+require("./models/category.model");
+require("./models/rating.model");
 
-const userModel = mongoose.model('user');
+const userModel = mongoose.model("user");
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 passport.use(
-  'local',
+  "local",
   new LocalStrategy(
-    { usernameField: 'username', passwordField: 'password' },
+    { usernameField: "username", passwordField: "password" },
     (username, password, done) => {
       userModel.findOne({ username: username }, (err, user) => {
-        if (err) return done('Error during request.', null);
+        if (err) return done("Error during request.", null);
         if (!user) return done(null, false);
         user.comparePasswords(password, (error, isMatch) => {
           if (error) return done(error, false);
@@ -73,12 +73,12 @@ passport.use(
 );
 
 passport.serializeUser((user, done) => {
-  if (!user) return done('No user provided', null);
+  if (!user) return done("No user provided", null);
   return done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
-  if (!user) return done('No user provided', null);
+  if (!user) return done("No user provided", null);
   return done(null, user);
 });
 
@@ -96,21 +96,22 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(require('./utils/inject-globals'));
-app.use('/auth', require('./routes/auth.routes'));
-app.use('/images', require('./routes/image.routes'));
-app.use('/users', ensureAuthenticated, require('./routes/user.routes'));
-app.use('/admin', require('./routes/admin.routes'));
-app.use('/api', require('./routes/api.routes'));
+app.use(require("./utils/inject-globals"));
+app.use("/auth", require("./routes/auth.routes"));
+app.use("/images", require("./routes/image.routes"));
+app.use("/users", ensureAuthenticated, require("./routes/user.routes"));
+app.use("/admin", require("./routes/admin.routes"));
+app.use("/api", require("./routes/api.routes"));
 
-app.get('/', (req, res) => {
-  return res.redirect('/images');
+app.get("/", (req, res) => {
+  return res.redirect("/images");
 });
 
 app.use((req, res, next) => {
-  res.status(404).send('Not found');
+  res.status(404).send("Not found");
 });
 
 app.listen(port, () => {
   console.log(`Server started on port`);
 });
+// new comment in index.js
